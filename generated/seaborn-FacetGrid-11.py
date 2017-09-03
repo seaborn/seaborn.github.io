801d2af91910748702239c00e89a570ec5b14904
mwaskom <mwaskom@stanford.edu>
2017-09-03 12:30:23 -0400

    Copy in v0.8.1 build directory

--- generated/seaborn-FacetGrid-11.py
+++ generated/seaborn-FacetGrid-11.py
@@ -1,4 +1,3 @@
 attend = sns.load_dataset("attention")
-g = sns.FacetGrid(attend, col="subject", col_wrap=5,
-                  size=1.5, ylim=(0, 10))
-g = g.map(sns.pointplot, "solutions", "score", scale=.7)
+g = sns.FacetGrid(attend, col="subject", col_wrap=5, size=1.5)
+g = g.map(plt.plot, "solutions", "score", marker=".")
--- generated/seaborn-FacetGrid-6.py
+++ generated/seaborn-FacetGrid-6.py
@@ -1,2 +1,2 @@
 g = sns.FacetGrid(tips, col="day", size=4, aspect=.5)
-g = g.map(sns.boxplot, "time", "total_bill")
+g = g.map(plt.hist, "total_bill", bins=bins)
