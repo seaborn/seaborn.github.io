789d6d723913ebcd023a4d19a46ec959425d0fa0
Michael Waskom <mwaskom@nyu.edu>
2020-09-09 17:54:29 -0400

    Add version warning to 0.10 docs and replace old static files

---
+++ versionwarning.js
@@ -0,0 +1,24 @@
+(function() {
+    const regex = /\d\.\d+/;
+    
+    var showWarning = (msg) => {
+        $('.content').prepend(
+            '<br>' +
+            '<p style="' +
+            [
+                'margin-top: 1px',
+                'padding: 1em',
+                'font-size: 1.2em',
+                'background: #ffeeee',
+            ].join('; ') +
+            '">' +
+            '<a href="https://seaborn.pydata.org/" style="color: #333">' +
+            'This is documentation for an old version. ' +
+            'Click here to load docs for the latest release.' +
+            '</a>' +
+            '</p>')
+    };
+    var versionPath = location.pathname.split('/')[1];
+    if (versionPath.match(regex)) { showWarning() }
+})()
+
